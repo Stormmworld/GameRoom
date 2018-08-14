e63a4439b5cc4fe1ff7b0f0941954829f7924ae0
Steve Corrado <steve.corrado@macys.com>
2018-08-13 20:52:30 -0400

    s

---
+++ Api/MTG/MTG/Interfaces/ICardEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MTG.Interfaces
+{
+    public interface ICardEventArgs
+    {
+        #region Properties
+        Guid CardId { get; set; }
+        Guid ControllerId { get; set; }
+        #endregion
+
+        #region Methods
+        string ToString();
+        #endregion
+    }
+}
